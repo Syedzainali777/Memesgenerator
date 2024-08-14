6cd72e183aa2b75a7d23adf909ee4ff9b756a5d1
Syedzainali777 <ali4205359@gmail.com>
2024-08-14 12:10:55 +0500

    add download button

--- src/Pages/editpage.jsx
+++ src/Pages/editpage.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Textofmeme from "../component/Textofmeme.jsx";
+import { exportComponentAsJPEG } from "react-component-export-image";
 
 const Editpage = () => {
   const [params] = useSearchParams();
   const [count, setCount] = useState(0);
+
+  // Use useRef instead of createRef
+  const memeRef = useRef();
 
   const addText = () => {
     setCount(count + 1); // Correctly updating the state
   };
 
   return (
-    <div>
-      <div>
+    <div className="container">
+      <div ref={memeRef}>
         <h1>Edit Page</h1>
         <img src={params.get("url")} alt="Meme" width="400px" />
         {Array(count)
@@ -21,7 +25,17 @@ const Editpage = () => {
             <Textofmeme key={index} /> // Each Text component needs a unique key
           ))}
       </div>
-      <button onClick={addText}>Add Text</button>
+      <button onClick={addText} className="btn btn-primary">
+        Add Text
+      </button>
+      <button
+        onClick={() => {
+          exportComponentAsJPEG(memeRef); // Export the component as JPEG
+        }}
+        className="btn btn-success"
+      >
+        Save
+      </button>
     </div>
   );
 };
--- src/component/Textofmeme.jsx
+++ src/component/Textofmeme.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import Draggable from "react-draggable";
+
 const Textedit = () => {
+  const [edit, setEdit] = useState(false);
+  const [val, setVal] = useState("double click to edit");
+
   return (
-    <div>
-      <h1>im am text</h1>
-    </div>
+    <Draggable>
+      <div>
+        {edit ? (
+          <input
+            value={val}
+            onChange={(e) => setVal(e.target.value)}
+            onBlur={() => setEdit(false)} // Exit edit mode when input loses focus
+          />
+        ) : (
+          <h1 onDoubleClick={() => setEdit(true)}>{val}</h1>
+        )}
+      </div>
+    </Draggable>
   );
 };
 

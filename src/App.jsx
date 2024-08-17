3e461ab3c85444dcd6d3e52757351bb9e78589e6
Syedzainali777 <ali4205359@gmail.com>
2024-08-17 13:03:56 +0500

    correct css path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Homepage from "./Pages/home.jsx";
 import Editpage from "./Pages/editpage.jsx";
 import { Routes, Route } from "react-router-dom";
-import '../src/app.css'
+import './App.css'
 
 function App() {
   return (
